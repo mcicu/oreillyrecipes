c9400f53c5763533c532299e655fd192512742bc
Mihai CICU <T0232181@ad-01.ent-01.adgroup>
2019-12-23 16:16:10 +0200

    added previous/next recipe navigation

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {RecipeService} from './services/recipe.service';
 import {LoggingService} from '../services/logging.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 
 @Component({
@@ -18,7 +18,8 @@ export class RecipesComponent implements OnInit, OnDestroy {
 
   constructor(private loggingService: LoggingService,
               private recipeService: RecipeService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
 
   }
 
@@ -39,5 +40,19 @@ export class RecipesComponent implements OnInit, OnDestroy {
   onRecipeSelected(recipe: Recipe) {
     this.loggingService.log('Selected recipe ' + recipe);
     this.selectedRecipe = recipe;
+  }
+
+  onNextRecipe() {
+    if (null !== this.selectedRecipe) {
+      const nextRecipeId: number = this.selectedRecipe.id + 1;
+      this.router.navigate(['/recipes', nextRecipeId], {queryParams: {navigatingUsingNext: true}});
+    }
+  }
+
+  onPrevRecipe() {
+    if (null !== this.selectedRecipe) {
+      const previousRecipeId: number = this.selectedRecipe.id - 1;
+      this.router.navigate(['/recipes', previousRecipeId], {queryParams: {navigatingUsingPrevious: true}});
+    }
   }
 }
