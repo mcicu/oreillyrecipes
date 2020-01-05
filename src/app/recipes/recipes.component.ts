437922b9ec79ca51cae0154e38abc909c132131f
Ana Tiru <tiru.ana21@gmail.com>
2020-01-05 12:04:35 +0200

    removed prev/next recipe logic

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Recipe} from './recipe.model';
 import {RecipeService} from './services/recipe.service';
 import {LoggingService} from '../services/logging.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
@@ -12,7 +12,7 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 })
 export class RecipesComponent implements OnInit, OnDestroy {
 
-  selectedRecipe: Recipe;
+  routeFirstChild: Subscription = new Subscription();
 
   constructor(private loggingService: LoggingService,
               private recipeService: RecipeService,
@@ -22,32 +22,17 @@ export class RecipesComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => {
-        console.log('Parametrii sunt' + params);
-      }
-    );
+    if (this.route.firstChild) {
+      this.routeFirstChild = this.route.firstChild.params.subscribe(
+        (params: Params) => {
+          console.log('Parametrii sunt', params);
+        }
+      );
+    }
   }
 
   ngOnDestroy(): void {
+    this.routeFirstChild.unsubscribe();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.loggingService.log('Selected recipe ' + recipe);
-    this.selectedRecipe = recipe;
-  }
-
-  onNextRecipe() {
-    if (null !== this.selectedRecipe) {
-      const nextRecipeId: number = this.selectedRecipe.id + 1;
-      this.router.navigate([nextRecipeId], {relativeTo: this.route, queryParams: {navigatingUsingNext: true}});
-    }
-  }
-
-  onPrevRecipe() {
-    if (null !== this.selectedRecipe) {
-      const previousRecipeId: number = this.selectedRecipe.id - 1;
-      this.router.navigate([previousRecipeId], {relativeTo: this.route, queryParams: {navigatingUsingNext: true}});
-    }
-  }
 }
